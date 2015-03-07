0c85dd5bb2260dcf164e2f07abd1a1361a96e4bc
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2015-03-07 16:15:59 +0100

    chat

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,9 +13,23 @@ angular.module('starter.controllers', [])
     };
 })
 
-.controller('ChatsCtrl', function ($scope, $firebaseObject) {
-    var ref = new Firebase("https://dazzling-fire-1486.firebaseio.com/");
-})
+.controller('ChatsCtrl', function ($scope, $firebaseObject)
+    {
+        var ref = new Firebase("https://dazzling-fire-1486.firebaseio.com/users/user1/coord");
+        $scope.marker = $firebaseObject(ref);
+        $scope.map = {
+            center: {
+                latitude: 48,
+                longitude: 16
+            },
+            zoom: 8
+        };
+        $scope.myPosition = {};
+        navigator.geolocation.getCurrentPosition(function(position) {
+            $scope.myPosition.longitude = position.coords.longitude;
+            $scope.myPosition.latitude = position.coords.latitude;
+        });
+    })
 
 .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
     $scope.chat = Chats.get($stateParams.chatId);
